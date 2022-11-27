328adfc7ca4c85ca4526ec7aeaa64513b66508be
Peacemaker84 <mazdat76@gmail.com>
2022-11-27 16:53:18 +0300

    Sad cats

--- script.js
+++ script.js
@@ -1,36 +1,41 @@
 const container = document.querySelector("main");
 const popupBlock = document.querySelector(".popup-wrapper");
+const popupAdd = popupBlock.querySelector(".popup-add");
+const popupUpd = popupBlock.querySelector(".popup-upd");
+const addForm = document.forms.addForm;
+const updForm = document.forms.updForm;
+const cards = document.getElementsByClassName("card");
 
-// let user = localStorage.getItem("catUser");
-// if (!user) {
-//   user = prompt("Представьтесь, пожалуйста");
-//   localStorage.setItem("catUser", user);
-// }
+document.getElementById("user").addEventListener("click", function () {
+  localStorage.removeItem("catUser");
+});
 
-popupBlock
-  .querySelector(".popup__close")
-  .addEventListener("click", function () {
+let user = localStorage.getItem("catUser");
+if (!user) {
+  user = prompt("Представьтесь, пожалуйста");
+  localStorage.setItem("catUser", user);
+}
+
+popupBlock.querySelectorAll(".popup__close").forEach(function (btn) {
+  btn.addEventListener("click", function () {
     popupBlock.classList.remove("active");
+    btn.parentElement.classList.remove("active");
+    if (btn.parentElement.classList.contains("popup-upd")) {
+      updForm.dataset.id = "";
+    }
   });
+});
 
 document.querySelector("#add").addEventListener("click", function (e) {
   e.preventDefault();
   popupBlock.classList.add("active");
+  popupAdd.classList.add("active");
 });
-
-const addForm = document.forms.addForm;
-/*
-	{
-		id, name, rate, age, 
-		img_link, description, 
-		favourite
-	}
-*/
 
 const createCard = function (cat, parent) {
   const card = document.createElement("div");
   card.className = "card";
-
+  card.dataset.id = cat.id;
   const img = document.createElement("div");
   img.className = "card-pic";
   if (cat.img_link) {
@@ -44,11 +49,11 @@ const createCard = function (cat, parent) {
   const name = document.createElement("h3");
   name.innerText = cat.name;
 
-  let like = "";
-  like.onclick = () => {
-    //....
-    // cat.id
-  };
+  // let like = "";
+  // like.onclick = () => {
+  //   //....
+  //   // cat.id
+  // };
 
   const del = document.createElement("button");
   del.innerText = "delete";
@@ -59,17 +64,33 @@ const createCard = function (cat, parent) {
     deleteCat(id, card);
   });
 
-  const editCat = document.createElement("button");
-  editCat.innerText = "edit";
-  editCat.classList.add("editCat");
-  editCat.name = cat.name;
-  editCat.addEventListener("click", function (elc) {
-    let name = elc.target.name;
-    editCat(name, card);
+  const upd = document.createElement("button");
+  upd.innerText = "update";
+  upd.classList.add("editCat");
+
+  upd.addEventListener("click", function (e) {
+    popupUpd.classList.add("active");
+    popupBlock.classList.add("active");
+    showForm(cat);
+    updForm.setAttribute("data-id", cat.id);
   });
 
-  card.append(img, name, del, editCat);
+  card.append(img, name, del, upd);
   parent.append(card);
+};
+
+const showForm = function (data) {
+  console.log(data);
+  for (let i = 0; i < updForm.elements.lenght; i++) {
+    let el = updForm.elements[i];
+    if (el.name) {
+      if (el.type !== "checkbox") {
+        el.value = data[el.name] ? data[el.name] : "";
+      } else {
+        el.checked = data[el.name];
+      }
+    }
+  }
 };
 
 // createCard({ name: "Вас", img_link: "" }, container);
@@ -164,7 +185,55 @@ addForm.addEventListener("submit", function (e) {
       body[el.name] = el.name === "favourite" ? el.checked : el.value;
     }
   }
-
   console.log(body);
   addCat(body);
 });
+
+updForm.addEventListener("submit", function (e) {
+  e.preventDefault();
+  let body = {};
+
+  for (let i = 0; i < this.elements.length; i++) {
+    let el = this.elements[i];
+    if (el.name) {
+      body[el.name] = el.name === "favourite" ? el.checked : el.value;
+    }
+  }
+  delete body.id;
+  console.log(body);
+  updCat(body, updForm.dataset.id);
+});
+
+const updCat = async function (obj, id) {
+  let res = await fetch(
+    `https://sb-cats.herokuapp.com/api/2/Peacemaker84/update/${id}`,
+    {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(obj),
+    }
+  );
+  let answer = await res.json();
+  console.log(answer);
+  if (answer.message === "ok") {
+    updCard(obj, id);
+    updForm.reset();
+    updForm.dataset.id = "";
+    popupUpd.classList.remove("active");
+    popupBlock.classList.remove("active");
+  }
+};
+
+const updCard = function (data, id) {
+  for (let i = 0; i < cards.lengthl; i++) {
+    let card = cards[i];
+    if (card[i].dataset.id === id) {
+      card.firstElementChild.style.backgroundImage = data.img_link
+        ? `url(${data.img_link})`
+        : `url(img/cat.png)`;
+      card.querySelector("h3").innerText = data.name || "noname";
+    }
+  }
+};
